03f80538964faafa10d2c565008cf31d9fd86a35
Ludovic Delafontaine <ludovic.delafontaine@gmail.com>
2017-03-26 19:59:40 +0200

    Improved Maven configuration

--- src/main/java/ch/tofind/commusica/Commusica.java
+++ src/main/java/ch/tofind/commusica/Commusica.java
@@ -1,11 +1,8 @@
 package ch.tofind.commusica;
 
-/**
- * Created by ludelafo on 3/26/17.
- */
 public class Commusica {
 
     public static void main(String[] args) {
-
+        System.out.println("App démarrée :)");
     }
 }
