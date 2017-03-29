5ec9a644e083ead147e2d84c749aa81fe9069e4a
Lucas Elisei <faku99dev@gmail.com>
2017-03-29 12:32:57 +0200

    Added upvote and downvote actions

--- src/main/java/ch/tofind/commusica/ui/TrackCellController.java
+++ src/main/java/ch/tofind/commusica/ui/TrackCellController.java
@@ -3,13 +3,21 @@ package ch.tofind.commusica.ui;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 public class TrackCellController {
 
+    private static final Logger LOG = Logger.getLogger(TrackCellController.class.getName());
+
     private final String FXML_FILE = "trackCell.fxml";
+
+    private String track;
+    private int votes;
 
     @FXML
     private AnchorPane trackPane;
@@ -19,6 +27,9 @@ public class TrackCellController {
 
     @FXML
     private Label titleLabel;
+
+    @FXML
+    private Label votesLabel;
 
     public TrackCellController(String track) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource(FXML_FILE));
@@ -30,10 +41,24 @@ public class TrackCellController {
             throw new RuntimeException(e);
         }
 
+        this.track = track;
+
         titleLabel.setText(track);
     }
 
     public AnchorPane getPane() {
         return trackPane;
+    }
+
+    @FXML
+    private void upvote(MouseEvent event) {
+        votesLabel.setText(String.valueOf(++votes));
+        LOG.info(String.format("%s upvoted!", track));
+    }
+
+    @FXML
+    private void downvote(MouseEvent event) {
+        votesLabel.setText(String.valueOf(--votes));
+        LOG.info(String.format("%s downvoted :(", track));
     }
 }
