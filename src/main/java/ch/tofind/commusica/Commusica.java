3bbfc27b4658d1ecdeed2855f2e97cf8e23b4794
Ludovic Delafontaine <ludovic.delafontaine@gmail.com>
2017-04-04 12:10:04 +0200

    Merged Track and DatabaseManager

--- src/main/java/ch/tofind/commusica/Commusica.java
+++ src/main/java/ch/tofind/commusica/Commusica.java
@@ -1,11 +1,26 @@
 package ch.tofind.commusica;
 
+import ch.tofind.commusica.database.DatabaseManager;
+import ch.tofind.commusica.media.Track;
 import ch.tofind.commusica.ui.UIController;
 
 public class Commusica {
 
     public static void main(String[] args) {
+        
         System.out.println("App démarrée :)");
+        Track track1 = new Track("Test1", "Test", "Test", 123, "/tmp/sample1.wav");
+        Track track2 = new Track("Test2", "Test", "Test", 132, "/tmp/sample2.wav");
+        Track track3 = new Track("Test3", "Test", "Test", 321, "/tmp/sample3.wav");
+
+        DatabaseManager.getInstance().save(track1);
+        DatabaseManager.getInstance().save(track2);
+        DatabaseManager.getInstance().save(track3);
+
+        System.out.println(track1);
+        System.out.println(track2);
+        System.out.println(track3);
+
         UIController.launch(UIController.class);
     }
 }
