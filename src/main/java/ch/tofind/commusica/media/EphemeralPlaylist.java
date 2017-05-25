697d9aff4105299ae5ba202cae7871bb6e08a669
Lucas Elisei <lucas.elisei@heig-vd.ch>
2017-05-25 19:04:54 +0200

    More cleanup

--- src/main/java/ch/tofind/commusica/media/EphemeralPlaylist.java
+++ src/main/java/ch/tofind/commusica/media/EphemeralPlaylist.java
@@ -103,9 +103,7 @@ public class EphemeralPlaylist implements IPlaylist {
     public boolean addTrack(Track track) {
         // Check that the playlist doesn't already contains the track.
         if (tracksList.stream().noneMatch(p -> p.getTrack().equals(track))) {
-            //if (PlaylistManager.getInstance().getPlaylist() == this) {
             DatabaseManager.getInstance().save(track);
-            //}
 
             // Create the PlaylistTrack object by associating it with the static playlist.
             PlaylistTrack playlistTrack = new PlaylistTrack(delegate, track);
